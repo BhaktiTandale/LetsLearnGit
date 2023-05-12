cb57c32121b3b5d8cd293a5fce1e10731b50c125
Bhakti Tandale <bhakti.tandale@alterahealth.com>
2023-05-12 14:26:41 +0530

    initial commit

---
+++ add.cs
@@ -0,0 +1,12 @@
+
+class Program
+  {
+    static void Main(string[] args)
+    {
+      int a = 10;
+    int b = 20;
+    int sum = a + b;
+
+        Console.WriteLine("Sum = " + sum);    
+    }
+  }
